cc5ce9c10abd7cff1c32cbb062155f6bbf86dc6b
KeyurMistry <keyurmist@gmail.com>
2022-09-10 01:05:57 +0100

    added function for a single thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,4 +9,16 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  //get single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };

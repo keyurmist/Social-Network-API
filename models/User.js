6c62f4045091691e9c65580ed1ef17e3999a5595
KeyurMistry <keyurmist@gmail.com>
2022-09-10 16:35:56 +0100

    finished user schema model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: [true, "That username is already in use!"],
+      required: [true, "Username should not be empty"],
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      unique: [true, "That email is already in use!"],
+      required: [true, "Email should not be empty"],
+      match: [
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email address.",
+      ],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+const User = model("User", userSchema);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+module.exports = User;
